778462ff02103088db9bcbb909834057d4c983f2
Armin Dizdarevic <adizdarevic96@gmail.com>
2019-03-29 20:49:01 -0400

    Add project files.

---
+++ Pastebin Scraper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pastebin_Scraper
+{
+    internal class Program
+    {
+        //Entry Point
+        private static void Main(string[] args)
+        {
+            
+        }
+    }
+}
