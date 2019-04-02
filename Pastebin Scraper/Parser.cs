dd1964320004e8e04e90de4898e4eba20eb8fe7f
Armin Dizdarevic <adizdarevic96@gmail.com>
2019-04-02 00:59:02 -0400

    Regex Implementation & Menu Demo
    - Basic implementation of regex search, currently unused but available. By default Regex search is set to false, and at the moment the menu does not have an option to enable it. Will require futher testing to make sure the regex search is accurate, will come at a later date. The Regex search can be enabled by setting isRegex bool param on Parser to true, at the moment set to always false.
    
    - Implemented a demo menu to allow a basic user interface for the application to run.

--- Pastebin Scraper/Parser.cs
+++ Pastebin Scraper/Parser.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Pastebin_Scraper
 {
@@ -7,23 +8,33 @@ namespace Pastebin_Scraper
     internal static class Parser
     {
         // Static function to parse a list of paste objects, along with a string to search for
-        public static void ParsePastes(List<Pastebin.Paste> pastes, string pattern)
+        public static void ParsePastes(List<Pastebin.Paste> pastes, string pattern, bool isRegex = false)
         {
             // Initializes new List of strings, for storing URLs later
             var urlList = new List<string>();
 
             // Iterates over each paste 
             foreach (var paste in pastes)
-                // Checks if individual paste contains search pattern string
-                if (paste.content.Contains(pattern))
-                    urlList.Add(paste.scrape_url);
+                // Separates Regex logic from normal string search logic
+                if (!isRegex)
+                {
+                    // Checks if individual paste contains search pattern string
+                    if (paste.content.Contains(pattern))
+                        urlList.Add(paste.scrape_url);
+                }
+                else
+                {
+                    // Checks if individual paste has regex pattern that matches, the one provided one function call
+                    if (Regex.IsMatch(paste.content, pattern, RegexOptions.IgnoreCase))
+                        urlList.Add(paste.scrape_url);
+                }
 
             // Passes list of URLS to function to be saved to text file
             SavePasteUrls(urlList);
         }
 
         // Function to save a list of URLs to a text file
-        private static void SavePasteUrls(List<string> urls)
+        private static void SavePasteUrls(IEnumerable<string> urls)
         {
             // Initializes a new disposable stream writer
             using (var writer = new StreamWriter("output.txt"))
--- Pastebin Scraper/Program.cs
+++ Pastebin Scraper/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 
 namespace Pastebin_Scraper
@@ -11,11 +12,59 @@ namespace Pastebin_Scraper
         //Entry Point
         private static void Main(string[] args)
         {
-            // Initializes a list of pastes to the return of the function scrape
-            var pastes = Scrape();
+            // Variables to be used later
+            var searchQuery = "";
+            var isRunning = false;
 
-            // Passes the return from the scrape function, a list of pastes, to the parser to be processed
-            Parser.ParsePastes(pastes, "");
+            // While not running the scraping logic, loop menu
+            while (!isRunning)
+            {
+                // Initial menu prompt + assignment of input selection
+                Console.Clear();
+                Console.WriteLine("-----Pastebin Scraper-----");
+                Console.WriteLine("1) Define Search Query");
+                Console.WriteLine("2) Start Scraping");
+                Console.Write("Please enter your selection: ");
+                var input = int.Parse(Console.ReadLine());
+
+                // Switching on the input provided by the user
+                switch (input)
+                {
+                    // Search query entry input
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("-----Pastebin Scraper-----");
+                        Console.Write("Please enter your Query: ");
+                        searchQuery = Console.ReadLine();
+                        break;
+                    // Start parsing input
+                    case 2:
+                        // Check to make sure we have a valid searchQuery
+                        if (!string.IsNullOrWhiteSpace(searchQuery))
+                        {
+                            isRunning = true;
+                            // Initializes a list of pastes to the return of the function scrape
+                            var pastes = Scrape();
+                            // Passes the return from the scrape function, a list of pastes, to the parser to be processed
+                            Parser.ParsePastes(pastes, searchQuery);
+                        }
+                        else
+                        {
+                            // Search query must be invalid, or undefined, so return to main menu
+                            Console.Clear();
+                            Console.WriteLine("-----Pastebin Scraper-----");
+                            Console.WriteLine("Error: You did not specify a search query.");
+                            Console.Write("Press any key to return to the main menu...");
+                            Console.ReadKey();
+                        }
+
+                        break;
+                    // Catching invalid input
+                    default:
+                        Console.Clear();
+                        break;
+                }
+            }
         }
 
         // Function to perform the general scraping of the latest public pastes from Pastebin
